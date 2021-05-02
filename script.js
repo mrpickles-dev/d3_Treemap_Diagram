9aba32ca76664cdc04b801e8f7d97cfc3c73d1c1
mrpickles-dev <mr.pickles300@gmail.com>
2021-05-02 23:28:04 +0200

    initial setup

---
+++ script.js
@@ -0,0 +1,21 @@
+let movieDataUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
+
+let movieData
+
+let canvas = d3.select('#canvas')
+
+let drawTreeMap = () => {
+
+}
+
+d3.json(movieDataUrl).then(
+    (data, error) => {
+        if(error){
+            console.log(error)
+        } else {
+            movieData = data
+            console.log(movieData)
+            drawTreeMap()
+        }
+    }
+)
